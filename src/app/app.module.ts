674dbcb0bb9b94ecc42baae022aa9f93c575dbfa
MonKeySVG <play95100@gmail.com>
2024-03-26 14:15:20 +0100

    fonctionnement basique du flip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SymbolComponent } from './symbol/symbol.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SymbolComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
